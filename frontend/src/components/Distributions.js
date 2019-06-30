3d867629f3c2e4ba4de72df09b552ccfd2d177ec
gsbevilaqua <gs_bevilaqua@hotmail.com>
2019-06-30 14:40:17 -0300

    Update Distributions.js

--- frontend/src/components/Distributions.js
+++ frontend/src/components/Distributions.js
@@ -13,7 +13,7 @@ export default class Details extends Component {
                 loved: "Loved",
                 hated: "Hated",
                 polarizer: "Polarizer",
-                more_polarizer: "More Polarizer"
+                more_polarizer: "Strongly Polarizer"
             },
             ratings: [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             dists: {
